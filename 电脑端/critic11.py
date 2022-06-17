e28448169af9df344e9e7f8b3d8b2f0a6f9b1419
Zheng ht <77187666+SUPER-99@users.noreply.github.com>
2022-06-17 16:37:52 +0800

    Add files via upload

---
+++ 电脑端/critic11.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import numpy as np
+import cv2
+import pickle
+from net import ANet, CNet
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = 'cpu'
+
+
+class CriticNode11(nn.Module):
+    def __init__(self):
+        super(CriticNode11, self).__init__()
+        # 定义完成功能所需的模�?
+        self.actor = ANet().to(device)
+        self.actor_target = ANet().to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.critic = CNet().to(device)
+        self.critic_target = CNet().to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+
+        self.discount = 0.99
+        self.tau = 0.001
+        self.actorDict = {}
+        self.criticDict = {}
+        print("初始化网络11完成")
+
+    def optimize(self, byteData):
+        print("开始优化网络11...")
+        # -----------self.critic(data)----------------
+        # to be modified:  model.train()
+        for _, sample in byteData.items():
+            img = torch.FloatTensor(sample["lastImg"]).unsqueeze(0).to(device)
+            info = torch.FloatTensor(sample["lastInfo"]).unsqueeze(0).to(device)
+            action = torch.FloatTensor(sample["a"]).to(device)
+            img_next = torch.FloatTensor(sample["img"]).unsqueeze(0).to(device)
+            info_next = torch.FloatTensor(sample["info"]).unsqueeze(0).to(device)
+            reward = torch.FloatTensor(sample["r"]).to(device)
+
+            # Compute the target Q value
+            target_Q = self.critic_target(img_next, info_next, self.actor_target(img_next, info_next))
+            target_Q = reward + (self.discount * target_Q).detach()
+            # print("target_Q", target_Q)
+
+            # Get current Q estimate
+            current_Q = self.critic(img, info, action)
+            # print("current_Q", current_Q)
+
+            # Compute critic loss and Optimize the critic
+            critic_loss = F.mse_loss(current_Q, target_Q)
+            # print("critic_loss", critic_loss)
+            self.critic.optimize(critic_loss)
+
+            # Compute actor loss and Optimize the actor
+            actor_loss = -self.critic(img, info, self.actor(img, info)).mean()
+            # print("actor_loss", actor_loss)
+            self.actor.optimize(actor_loss)
+            # -----------self.critic(data)----------------
+
+            # 读取并保存神经网络参数
+            self.actorDict = self.actor.state_dict()
+            # print("actor网络参数已获取", len(pickle.dumps(self.actorDict)))
+            self.criticDict = self.critic.state_dict()
+            torch.save(self.criticDict, "{}/{}.dict".format("dict/dict11", "criticDict"))
+            torch.save(self.actorDict, "{}/{}.dict".format("dict/dict11", "actorDict"))
+
+        # 更新target网络
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        self.load("dict/dict11", "criticDict", "actorDict")
+
+    def load(self, directory, filenameC, filenameA):
+        self.critic.load_state_dict(torch.load("{}/{}.dict".format(directory, filenameC), map_location=device))
+        self.actor.load_state_dict(torch.load("{}/{}.dict".format(directory, filenameA), map_location=device))
+        self.critic.eval()
+        print("Critic网络参数已加载")
---
+++ 电脑端/get_reward.py
@@ -0,0 +1,206 @@
+import numpy
+from math import *
+
+
+# Reward = R_Forward + R_CAvo + R_VKeep
+
+class Reward:
+    def __init__(self, coordinate, last_coordinate, last_a, a, all_angle):
+        self.rf = 0
+        self.rv = 0
+        self.rc = 0
+        self.rb = 0
+        # 相邻两次位置信息
+        self.coord = coordinate
+        self.last_coord = last_coordinate
+        # 小车两次速度和最新时刻的角度
+        self.vu = a[0][0]
+        self.last_vu = last_a[0][0]
+        self.angle = a[0][1]
+        # 障碍物速度
+        self.v_obs = 0
+        # 总的偏转角度
+        self.total_angle = all_angle * (180 / pi)
+        # 两次的时间
+        self.time = coordinate[0][1]
+        self.last_time = last_coordinate[0][1]
+        # 小车加速度
+        self.au = (self.vu - self.last_vu) / (self.time - self.last_time)
+
+    def get_rf(self):
+        angle = 40
+        xie = 0.25
+        # 计算车辆正向性奖励
+        if not self.coord:
+            loc_user_e = self.last_coord[0][0][0]
+        else:
+            loc_user_e = self.coord[0][0][0]
+        # 四个轮胎
+        # 右转
+        # print('total_angle', self.total_angle)
+        if self.total_angle <= 0:
+            # print('右转')
+            total_angle = - self.total_angle
+            # 右上
+            if self.total_angle + angle < 90:
+                ru = [loc_user_e[0] + sin((angle + total_angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((angle + total_angle) * (pi / 180)) * xie]
+            else:
+                ru = [loc_user_e[0] + sin((total_angle - 50) * (pi / 180)) * xie,
+                      loc_user_e[1] - cos((total_angle - 50) * (pi / 180)) * xie]
+            # 左上
+            if self.total_angle < angle:
+                lu = [loc_user_e[0] - sin((angle - total_angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((angle - total_angle) * (pi / 180)) * xie]
+            else:
+                lu = [loc_user_e[0] + sin((total_angle - angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((total_angle - angle) * (pi / 180)) * xie]
+
+            # 右下
+            if self.total_angle < angle:
+                rb = [loc_user_e[0] + xie * sin((angle - total_angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((angle - total_angle) * (pi / 180))]
+            else:
+                rb = [loc_user_e[0] - xie * sin((total_angle - angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((total_angle - angle) * (pi / 180))]
+
+            # 左下
+            if self.total_angle + angle < 90:
+                lb = [loc_user_e[0] - xie * sin((angle + total_angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((angle + total_angle) * (pi / 180))]
+            else:
+                lb = [loc_user_e[0] - xie * cos((total_angle - 50) * (pi / 180)),
+                      loc_user_e[1] + xie * sin((total_angle - 50) * (pi / 180))]
+        # 左转
+        else:
+            # print('左转')
+            total_angle = self.total_angle                      
+          # 右上
+            if self.total_angle < angle:
+                ru = [loc_user_e[0] + sin((angle - total_angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((angle - total_angle) * (pi / 180)) * xie]
+            else:
+                ru = [loc_user_e[0] - sin((total_angle - angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((total_angle - angle) * (pi / 180)) * xie]
+            # 左上
+            if self.total_angle + angle < 90:
+                lu = [loc_user_e[0] - sin((angle + total_angle) * (pi / 180)) * xie,
+                      loc_user_e[1] + cos((angle + total_angle) * (pi / 180)) * xie]
+            else:
+                lu = [loc_user_e[0] - cos((total_angle - 50) * (pi / 180)) * xie,
+                      loc_user_e[1] - sin((total_angle - 50) * (pi / 180)) * xie]
+
+            # 右下
+            if self.total_angle + angle < 90:
+                rb = [loc_user_e[0] + xie * sin((total_angle + angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((total_angle + angle) * (pi / 180))]
+            else:
+                rb = [loc_user_e[0] + xie * cos((total_angle - 50) * (pi / 180)),
+                      loc_user_e[1] + xie * sin((total_angle - 50) * (pi / 180))]
+
+            # 左下
+            if self.total_angle < angle:
+                lb = [loc_user_e[0] - xie * sin((angle - total_angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((angle - total_angle) * (pi / 180))]
+            else:
+                lb = [loc_user_e[0] + xie * sin((total_angle - angle) * (pi / 180)),
+                      loc_user_e[1] - xie * cos((total_angle - angle) * (pi / 180))]
+
+        # 障碍物位置
+        loc_obs = [0.3233132809043666, 0.6688269915604095]
+        distance1 = ((loc_obs[0] - ru[0]) ** 2 + (loc_obs[1] - ru[1]) ** 2) ** 0.5
+        distance2 = ((loc_obs[0] - lu[0]) ** 2 + (loc_obs[1] - lu[1]) ** 2) ** 0.5
+        # 碰撞奖励
+        if distance1 <= 0.2 or distance2 <= 0.2:
+            self.rc = -0.5
+        else:
+            self.rc = 0.1
+
+        # 边界奖励, x轴
+        b_l = -0.6
+        b_r = 0.6
+        print('car', loc_user_e)
+        print('ru', ru)
+        print('lu', lu)
+        if rb[0] > b_r or ru[0] > b_r or lb[0] < b_l or lu[0] < b_l:
+            self.rf = -0.5
+        else:
+            self.rf = 0.1
+
+        # 沿车道中心线行走
+        k2 = 0.5
+        if b_l < lu[0] < ru[0] < 0:
+            self.rb = k2 * (1 - (abs(loc_user_e[0]) / 0.3))
+        elif 0 < lu[0] < ru[0] < b_r:
+            self.rb = k2 * (1 - (abs(loc_user_e[0]) / 0.3))
+        else:
+            self.rb = 0
+
+        return self.rf, self.rc, self.rb
+
+    def get_rv(self):
+        # 计算车辆速度保持奖励
+        # goal 是否达到目的地
+        # goal = 0
+        # 该道路的最大速度
+        vmax = 1
+        # w自定义
+        w = 5
+        if self.vu < vmax:
+            self.rv = w * self.vu
+        elif self.vu > vmax:
+            self.rv = -0.5
+        return self.rv
+
+    def get_rc(self):
+        # 避障奖励
+        # 小车反应时间,自定义
+        react_time = 0.01
+        # 自定义
+        k = 1
+        # 最小安全距离
+        d_safe = 0
+        # 折损因子
+        k1 = 0.1
+
+        # 车与障碍物之间的距离
+        if not self.coord:
+            loc_user_e = self.last_coord[0][0][0]
+        else:
+            loc_user_e = self.coord[0][0][0]
+        
+        loc_obs = [0.3233132809043666, 0.6688269915604095]
+        distance = ((loc_obs[0] - loc_user_e[0]) ** 2 + (loc_obs[1] + 0.3 - loc_user_e[1]) ** 2) ** 0.5
+
+        # 理论上的安全距离
+        losd = (((self.vu * cos(self.total_angle)) ** 2) / (2 * self.au * cos(self.total_angle))) + (self.v_obs * react_time) + d_safe
+        print('加速度', self.au)
+        print('losd', losd)
+        v_hengxiang = self.vu * sin(self.total_angle)
+        print('total_angle', self.total_angle)
+        print('横向速度', v_hengxiang)
+        lasd = v_hengxiang ** 2 / self.au * sin(self.total_angle)
+        print('lasd', lasd)
+        safe_distance = (((losd ** 2) + (lasd ** 2)) ** 1 / 2)
+        print(distance, safe_distance)
+        if distance - 0.3 < safe_distance:
+            self.rc = - k * (safe_distance - distance) / safe_distance
+        if distance == 0:
+            self.rc = -0.5
+        else:
+            self.rc = 0.1
+        return self.rc
+
+    def reward_sum(self):
+        rv = self.get_rv()
+        rf, rc, rb = self.get_rf()
+        print('rc', rc)
+        # print('rv', rv)
+        print('rf', rf)
+        print('rb', rb)
+        return [rv+rc+rf+rb]
+
+
+
+
+
